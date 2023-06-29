54820fed86d678093c05283731999e249b28378c
Paul Rayer <paulrayer61@gmail.com>
2023-06-29 16:27:14 -0400

    add slider + imageCount my image

--- front/src/components/MyImage.vue
+++ front/src/components/MyImage.vue
@@ -1,10 +1,17 @@
 <script setup>
+import { ref } from 'vue'
+const imageCount = ref(null)
+
 
 </script>
 
 <template>
 	<div>
-		<h1>Gallery</h1>
+		<h1>My Image</h1>
+
+		<v-slider v-model="imageCount" :min="0" :max="20" :step="1" thumb-label max-height="400" :label="imageCount">
+		</v-slider>
+
 		<v-row>
 			<v-col v-for="img in images" :key="img" cols="2">
 				<v-card class="d-flex flex-column" variant="elevated">
