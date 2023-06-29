4487fc9bf53be84041209459650cdc99f1ad2960
Lucas <contact@lucasquitman.fr>
2023-06-29 18:18:58 -0400

    working gallery with recent images uploaded

--- front/src/components/Gallery.vue
+++ front/src/components/Gallery.vue
@@ -1,15 +1,44 @@
+
 <script setup>
+import { computed, ref, watch } from 'vue'
 defineOptions({
     name: "Gallery"
 })
+const imageAmount = ref(0)
+const images = ref([])
+async function getImages() {
+    const requestOptions = {
+        method: "GET",
+        credentials: "include",
+    };
+    try {
+        const response = await fetch(import.meta.env.VITE_API_URL + "/image?amount=" + imageAmount.value, requestOptions);
+        if (response.ok) {
+            const data = await response.json()
+            return data.images
+        } else {
+            console.error('An error has occured while using image function in')
+        }
+    } catch (error) {
+        console.error('an error has occured while fetching images', error)
+    }
+}
+
+watch(imageAmount, async (newValue, oldValue) => {
+    console.log('imageAmount changed from', oldValue, 'to', newValue)
+    const fetchedImages = await getImages()
+    images.value = fetchedImages
+    console.log(images.value)
+})
+imageAmount.value = 10
+
 </script>
 
 <template>
+    <v-slider v-model="imageAmount" :min="0" :max="30" :step="1" thumb-label></v-slider>
     <v-row>
-        <v-col v-for="n in 9" :key="n" class="d-flex child-flex" cols="4">
-            <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-                :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`" aspect-ratio="1" cover
-                class="bg-grey-lighten-2">
+        <v-col v-for="image in images" :key="_id" class="d-flex child-flex" cols="4">
+            <v-img :src="image.link" aspect-ratio="1" cover class="bg-grey-lighten-2">
                 <template v-slot:placeholder>
                     <v-row class="fill-height ma-0" align="center" justify="center">
                         <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
